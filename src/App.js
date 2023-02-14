53a46bf0fdeb01efc1827a4d29f38e6e3e2e314f
BamideleMo <bamidelemo@gmail.com>
2023-02-14 03:10:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={
+            <div className="text-center my-24 space-x-4 drop-shadow-lg p-6">
+              <div className="text-center">
+                <img src="JETS-logo.png" alt="Logo" className="inline w-20 animate-bounce" />
+              </div>
+              <h2 className=" my-12 text-2xl text-blue-900 font-semibold">Portal Currently Undegoing Routine Maintenance</h2>
+              <h3 className="font-semibold">We should be back online:<br />Wednesday, 15-02-2023</h3>
+              <p className="mt-4">For more information please contact the ICT department. Thank you for your patience.</p>
+            </div>
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
